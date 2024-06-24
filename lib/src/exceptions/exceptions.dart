9c5dea3db6821a433957f268e2cb45efc293479b
Philipp Lang <11468252+Pleanog@users.noreply.github.com>
2024-06-24 13:08:59 +0200

    Added statusCodes to the exeptionhandling
    Added statisCodes and a static method to convert status codes to descriptions and updated exception classes

--- lib/src/exceptions/exceptions.dart
+++ lib/src/exceptions/exceptions.dart
@@ -1,18 +1,48 @@
 class DORMException implements Exception {
   final String? message;
+  final int? statusCode;
 
-  DORMException({this.message});
+  DORMException({this.message, this.statusCode});
+
+  // Add a getter for the description based on the status code
+  String get description => DORMStatusCodes.getDescription(statusCode);
 
   @override
   String toString() {
-    return 'DORMException: $message';
+    return 'DORMException: [$statusCode] $description, $message';
   }
 }
 
-class DROMServerException extends DORMException {
-  DROMServerException({super.message});
+class DORMServerException extends DORMException {
+  DORMServerException({super.message, super.statusCode});
 }
 
-class DROMClientException extends DORMException {
-  DROMClientException({super.message});
+class DORMClientException extends DORMException {
+  DORMClientException({super.message, super.statusCode});
+}
+
+class DORMStatusCodes {
+  static const int invalidRequest = 400;
+  static const int unauthorized = 401;
+  static const int forbidden = 403;
+  static const int notFound = 404;
+  static const int internalServerError = 500;
+
+  // Static method to get the description based on the status code
+  static String getDescription(int? statusCode) {
+    switch (statusCode) {
+      case invalidRequest:
+        return 'Invalid Request';
+      case unauthorized:
+        return 'Unauthorized Request';
+      case forbidden:
+        return 'Forbidden';
+      case notFound:
+        return 'Not Found';
+      case internalServerError:
+        return 'Internal Server Error';
+      default:
+        return 'Unknown Error';
+    }
+  }
 }
